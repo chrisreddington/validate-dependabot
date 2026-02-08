d8f276243625db23f3565c49f9dcc6fc6dd2d4a4
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2026-02-08 19:25:46 +0000

    refactor: replace NCC with Rollup for packaging and add necessary plugins
    - Updated package script to use Rollup for building the project.
    - Added Rollup configuration file with TypeScript, CommonJS, and Node resolve plugins.
    - Removed @vercel/ncc dependency and added Rollup and its plugins as dependencies.

---
+++ rollup.config.ts
@@ -0,0 +1,28 @@
+// See: https://rollupjs.org/introduction/
+
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
+const config = {
+  input: 'src/index.ts',
+  output: {
+    esModule: true,
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true
+  },
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+      compilerOptions: {
+        module: 'ESNext',
+        moduleResolution: 'bundler'
+      }
+    }),
+    nodeResolve({ preferBuiltins: true }),
+    commonjs()
+  ]
+}
+
+export default config
